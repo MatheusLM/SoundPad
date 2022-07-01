056c8bcceb71b943cda06ba39d22b85ff6c0d807
Matheus Lopes Marques <2000ttu@gmail.com>
2022-06-30 23:09:24 -0300

    [Create] First modifications to create a base project

--- src/main.ts
+++ src/main.ts
@@ -2,42 +2,45 @@ import { app, BrowserWindow } from "electron";
 import * as path from "path";
 
 function createWindow() {
-  // Create the browser window.
-  const mainWindow = new BrowserWindow({
-    height: 600,
-    webPreferences: {
-      preload: path.join(__dirname, "preload.js"),
-    },
-    width: 800,
-  });
+	// Create the browser window.
+	const mainWindow = new BrowserWindow({
+		webPreferences: {
+			preload: path.join(__dirname, "preload.js"),
+		},
+		width: 720,
+		height: 480,
+		minWidth: 240,
+		minHeight: 220,
+	});
 
-  // and load the index.html of the app.
-  mainWindow.loadFile(path.join(__dirname, "../index.html"));
+	// and load the index.html of the app.
+	mainWindow.loadFile(path.join(__dirname, "../index.html"));
+	mainWindow.setPosition(2600, 200);
 
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+	// Open the DevTools.
+	/* mainWindow.webContents.openDevTools(); */
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.on("ready", () => {
-  createWindow();
+	createWindow();
 
-  app.on("activate", function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow();
-  });
+	app.on("activate", function () {
+		// On macOS it's common to re-create a window in the app when the
+		// dock icon is clicked and there are no other windows open.
+		if (BrowserWindow.getAllWindows().length === 0) createWindow();
+	});
 });
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
 app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") {
-    app.quit();
-  }
+	if (process.platform !== "darwin") {
+		app.quit();
+	}
 });
 
 // In this file you can include the rest of your app"s specific main process
